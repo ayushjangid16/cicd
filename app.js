c3a11e686e0f9a6ec960633ee77ac70a595d55b4
Ayush Jangid <ayush.jangid@helpfulinsightsolution.com>
2026-05-07 11:24:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World",
+    metadata: "I am from Local host",
+  });
+});
+
+app.listen(4545, () => {
+  console.log("Server is running on port 4545");
+});
