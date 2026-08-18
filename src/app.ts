3202c2cc317d2ee1b842153dc24f631f32e08869
Ayush Jangid <ayush.jangid@helpfulinsightsolution.com>
2026-08-18 10:26:37 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  return res.json({ message: 'Hello Implementing the CI part now' 
-    
-  });
+  const a: number = 'Hello';
+  return res.json({ message: 'Hello Implementing the CI part now' });
 });
 
 app.listen(4545, () => {
