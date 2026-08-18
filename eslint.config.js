0e20f429699e002a08b393c2e1373298e5a21478
Ayush Jangid <ayush.jangid@helpfulinsightsolution.com>
2026-08-18 10:16:50 +0530

    added ts

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+      globals: {
+        // Node.js globals
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({
+    message: 'Hello Implementing the CI part now',
+  });
+});
+
+app.listen(4545, () => {
+  console.log('app is running on port 4545');
+});
